e8212ca364f09cb544e1c27a23148a50f6d468a3
USCjames <james.rader@cox.net>
2019-04-01 23:45:05 -0700

    Almost Done

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBOqQjVnHSX8X-5NxuiBHlZr6dH3ZFWVWc",
+    authDomain: "trainschedule-142ec.firebaseapp.com",
+    databaseURL: "https://trainschedule-142ec.firebaseio.com",
+    projectId: "trainschedule-142ec",
+    storageBucket: "trainschedule-142ec.appspot.com",
+    messagingSenderId: "1095282002423"
+};
+
+firebase.initializeApp(config);
